7c7616f6ca838af7d9b421a9a551cf8f60c6e298
user.email <lanllierarthur@gmail.com>
2026-07-27 12:48:50 +0200

    docs: Module 3 : types numériques, représentation

---
+++ module_3_types_numeriques_representation/types.py
@@ -0,0 +1,9 @@
+from decimal import Decimal
+print(Decimal("0.1") + Decimal("0.2"))
+
+print(0.1 + 0.2)
+print(0.1 + 0.2 == 0.3)
+
+print(True == 1)
+print(True + True)
+print(isinstance(True, int))
