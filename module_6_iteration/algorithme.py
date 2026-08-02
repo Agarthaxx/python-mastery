acd0f1b19a182b5ebcc6a76ea543ed28a565ad74
user.email <lanllierarthur@gmail.com>
2026-08-03 01:49:02 +0200

    docs: Module 6 : iteration for/while, break/continue, for..else

--- module_6_iteration/algorithme.py
+++ module_6_iteration/algorithme.py
@@ -11,4 +11,23 @@ for x in billets:
         break
 else:
     print("Traitement terminé sans incident.")
-    
+
+# Alternative Booléen
+
+billets = ["valide", "valide", "annulé", "erreur", "valide", "valide"]
+
+erreur_rencontree = False
+
+for x in billets:
+    if x == "valide":
+        print(x)
+        continue
+    elif x == "annulé":
+        print(x)
+        continue
+    else:
+        erreur_rencontree = True
+        break
+
+if not erreur_rencontree:
+    print("Traitement terminé sans incident.")
